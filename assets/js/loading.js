7b13f043397d51e78b123f59a829a684b5a3538e
dimuthx353 <dimuthx353@gmail.com>
2025-02-20 01:14:50 +0530

    add dices

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -4,7 +4,7 @@
 var bar = new ProgressBar.Line(container, {
   strokeWidth: 4,
   easing: 'easeInOut',
-  duration: 30000,
+  duration: 9000,
   color: '#FFEA82',
   trailColor: '#eee',
   trailWidth: 1,
@@ -32,6 +32,48 @@ var bar = new ProgressBar.Line(container, {
 
 bar.animate(1.0); // Number from 0.0 to 1.0
 
-// setTimeout(() => {
-//   location.href = 'google.com';
-// }, 30000);
+setTimeout(() => {
+  document.querySelector('.loading').classList.add('d-none');
+}, 10000);
+
+// player logic
+const rollBtn = document.getElementById('roll-btn');
+let playerOne = 0;
+let playerTwo = 0;
+
+const playerOneScoreEl = document.getElementById('player-1');
+const playerTwoScoreEl = document.getElementById('player-2');
+const diceContainer = document.querySelector('.dice-container');
+const dices = diceContainer.querySelectorAll('div');
+let currentPlayerPlayerOne = true;
+
+updateScore();
+
+rollBtn.addEventListener('click', () => {
+  const number = Math.floor(Math.random() * 6) + 1;
+
+  if (currentPlayerPlayerOne) {
+    playerOne += number;
+    updateScore();
+  } else {
+    playerTwo += number;
+    updateScore();
+  }
+
+  if (number == 1) {
+    currentPlayerPlayerOne = true;
+  } else {
+    currentPlayerPlayerOne = !currentPlayerPlayerOne;
+  }
+
+  dices.forEach((dice) => {
+    dice.classList.add('d-none');
+  });
+
+  dices[number - 1].classList.remove('d-none');
+});
+
+function updateScore() {
+  playerOneScoreEl.textContent = playerOne;
+  playerTwoScoreEl.textContent = playerTwo;
+}
